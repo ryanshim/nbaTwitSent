5e426a9fd4f026d3404fddcacac5e5ef7593e0d7
Ryan <ryan.shim@gmial.com>
2018-02-14 19:17:18 -0800

    added comments

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
+// Server backend script
+// This script includes the app routing for other pages
+// we would like to serve.
+
 // Dependencies
 var express = require("express");
 var app = express();
 
 // Index page route
 app.get('/', function (req, res) {
-    res.sendFile('index.html', { root : '../public/model/' });
-});
-
+    res.sendFile('index.html', { root : '../public/model/' });  // setting the root dir will have express
+});                                                             // start looking for the file in that particular dir
 
 app.listen(5000);
 console.log('Server is listening')
