1deb9eed46c52ac2c2902556ffaad51a860fd71e
Mark Yang <mark.h.yang@gmail.com>
2018-03-05 12:43:12 -0800

    Added a logger

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,10 +1,27 @@
 import sqlite3
 import json
-import time
+import logging
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy import StreamListener
 from credentials import *
+from logger import *
+
+tracking = {
+    # Western
+    "@houstonrockets": "rockets", "#rockets": "rockets",
+    "@warriors": "warriors", "#dubnation": "warriors",
+    "@trailblazers": "blazers", "#ripcity": "blazers",
+    "@pelicansnba": "pelicans", "#doitbig": "pelicans",
+    "@timberwolves": "timberwolves", "#alleyesnorth": "timberwolves",
+
+    # Eastern
+    "@raptors": "raptors", "#wethenorth": "raptors",
+    "@celtics": "celtics", "#celtics": "celtics",
+    "@cavs": "cavaliers", "#allforone": "cavaliers",
+    "@pacers": "pacers", "#gopacers": "pacers",
+    "@washwizards": "wizards", "dcfamily": "wizards"
+}
 
 # connect to the database
 conn = sqlite3.connect("../data/data.db", uri=True)
@@ -17,39 +34,33 @@ auth.set_access_token(access_token, access_token_secret)
 # create a stream listener
 class listener(StreamListener):
     def on_data(self, data):
-        if 'event' in data: # only handle status posts, ignore all other events
-            all_data = json.loads(data)
+        all_data = json.loads(data)
 
+        if "retweeted_status" not in all_data \
+        and "RT @" not in all_data["text"] \
+        and "rt @" not in all_data["text"]:
             date = all_data["created_at"] # maybe use timestamp_ms
             user = all_data["user"]["screen_name"]
             tweet = all_data["text"]
+            if all_data["truncated"]:
+                tweet = all_data["extended_tweet"]["full_text"]
 
-            # TODO: figure out how to extract the search tag and get the team
-            # TODO: filter out repeats?
-            t = (date, "team", "tag", user, tweet, None)
+            t = (date, "", "", user, tweet, None)
+            for k, v in tracking.items():
+                if k in tweet:
+                    t = (date, v, k, user, tweet, None)
 
             c.execute("INSERT INTO tweets VALUES (?,?,?,?,?,?)", t)
             conn.commit()
+            logger.info("Stored tweet")
+        logger.debug(all_data)
 
     def on_error(self, status_code):
         # TODO: change credentials or restart process in an hour
+        print(status_code)
         if status_code == 420: # hit the Twitter rate limit
             return False
 
 # start tracking tweets
 twitterStream = Stream(auth, listener())
-twitterStream.filter(languages="en", track=[
-    # Western
-    "@houstonrockets", "#rockets",
-    "@warriors", "#dubnation",
-    "@trailblazers", "#ripcity",
-    "@pelicansnba", "#doitbig",
-    "@timberwolves", "#alleyesnorth",
-
-    # Eastern
-    "@raptors", "#wethenorth",
-    "@celtics", "#celtics",
-    "@cavs", "#allforone",
-    "@pacers", "#gopacers",
-    "@washwizards", "dcfamily"
-])
+twitterStream.filter(languages=["en"], track=tracking.keys())
