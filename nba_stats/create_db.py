1f70e81977b6d76d3e4ead1b0486358a3364aa7e
ryanshim <ryan.shim@gmail.com>
2018-03-16 16:42:25 -0700

    added nba stats collection script

--- nba_stats/create_db.py
+++ nba_stats/create_db.py
@@ -9,6 +9,9 @@ c = conn.cursor()
 c.execute('''CREATE TABLE teams 
                 (team text, w real, l real, pct real)''')
 
+c.execute('''CREATE TABLE players
+                (team text, num real, name text, pos text, height text, weight real)''')
+
 conn.commit()   # save changes
 
 conn.close()
--- nba_stats/nba_stats.py
+++ nba_stats/nba_stats.py
@@ -16,7 +16,7 @@ def populate_teams():
         tid = TEAMS[team_id]['id']
         team_stats = dict(team.TeamSummary(tid, season).json)
 
-        team_name = team_stats['resultSets'][0]['rowSet'][0][3]
+        team_name = team_id 
         team_wins = team_stats['resultSets'][0]['rowSet'][0][8]
         team_loss = team_stats['resultSets'][0]['rowSet'][0][9]
         team_pct = team_stats['resultSets'][0]['rowSet'][0][10]
@@ -27,9 +27,28 @@ def populate_teams():
 
     conn.close()
 
+def populate_players():
+    conn = sqlite3.connect('data_team.db')
+    c = conn.cursor()
 
-def main():
-    populate_teams()
+    for team_id in team_ids:
+        tid = TEAMS[team_id]['id']
+        player_stats = dict(team.TeamCommonRoster(tid, season).json)
+
+        for player in player_stats['resultSets'][0]['rowSet']:
+            num = player[4]
+            name = player[3]
+            pos = player[5]
+            height = player[6]
+            weight = player[7]
+
+            row = (team_id, num, name, pos, height, weight)
+            c.execute('INSERT INTO players VALUES (?,?,?,?,?,?)', row)
+            conn.commit()
+
+    conn.close()
 
 if __name__ == '__main__':
-    main()
+    populate_teams()
+    populate_players()
+
