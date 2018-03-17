04c7e32fb535690caa571e19f58a943755acaa3a
Mark Yang <mark.h.yang@gmail.com>
2018-03-17 12:27:10 -0700

    Moved front-end stuff to app folder

--- app/server.js
+++ app/server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
 
+var options = {
+    root: __dirname, 
+    dotfiles: 'deny'
+};
+var options_static = {
+  root: __dirname + "/static/",
+  dotfiles: 'ignore',
+  index: false,
+  redirect: false,
+}
+
+app.use(express.static('static', options_static));
+
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.sendFile('index.html', options);
 });
 
 app.listen(5000);
