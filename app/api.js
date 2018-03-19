78012f5bc8575e0296fa98d5aa2fe5db0c09d54b
Mark Yang <mark.h.yang@gmail.com>
2018-03-18 17:17:22 -0700

    Change let to var

--- app/api.js
+++ app/api.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res) {
 /api/:team/tweetscore\treturns a team's average twitter sentiment score\n\
 /api/:team/tweetscore/day\treturns a team's average twitter sentiment score by day\n\
 /api/:team/tweetscore/hour\treturns a team's average twitter sentiment score by hour\n\n\
-replace :team with the 3 letter code\n\
+replace :team with the 3 varter code\n\
 BOS Boston Celtics\n\
 CLE Cleveland Cavaliers\n\
 GSW Golden State Warriors\n\
@@ -32,68 +32,68 @@ WAS Washington Wizards\n\
 });
 
 router.get('/roster', function(req, res) {
-    let sql = "SELECT * FROM players";
+    var sql = "SELECT * FROM players";
     var params = [];
     stats_query(sql, params, res);
 });
 router.get('/record', function(req, res) {
-    let sql = "SELECT * FROM teams";
+    var sql = "SELECT * FROM teams";
     var params = [];
     stats_query(sql, params, res);
 });
 router.get('/tweetscore/hour', function(req, res) {
-    let sql = "SELECT team, datetime(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets GROUP BY team, datetime(date)";
+    var sql = "SELECT team, datetime(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets GROUP BY team, datetime(date)";
     var params = [];
     tweets_query(sql, params, res);
 });
 router.get('/tweetscore/day', function(req, res) {
-    let sql = "SELECT team, date(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets GROUP BY team, date(date)";
+    var sql = "SELECT team, date(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets GROUP BY team, date(date)";
     var params = [];
     tweets_query(sql, params, res);
 });
 router.get('/tweetscore', function(req, res) {
-    let sql = "SELECT team, AVG(score) as sentiment, COUNT(score) as count FROM tweets GROUP BY team";
+    var sql = "SELECT team, AVG(score) as sentiment, COUNT(score) as count FROM tweets GROUP BY team";
     var params = []
     tweets_query(sql, params, res);
 });
 router.get('/:team/roster', function(req, res) {
-    let sql = "SELECT * FROM players WHERE team = ?";
+    var sql = "SELECT * FROM players WHERE team = ?";
     var params = [req.params.team.toUpperCase()];
     stats_query(sql, params, res);
 });
 router.get('/:team/record', function(req, res) {
-    let sql = "SELECT * FROM teams WHERE team = ?";
+    var sql = "SELECT * FROM teams WHERE team = ?";
     var params = [req.params.team.toUpperCase()];
     stats_query(sql, params, res);
 });
 router.get('/:team/tweetscore/hour', function(req, res) {
-    let sql = "SELECT team, datetime(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets WHERE team = ? GROUP BY datetime(date)";
+    var sql = "SELECT team, datetime(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets WHERE team = ? GROUP BY datetime(date)";
     var params = [req.params.team.toUpperCase()];
     tweets_query(sql, params, res);
 });
 router.get('/:team/tweetscore/day', function(req, res) {
-    let sql = "SELECT team, date(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets WHERE team = ? GROUP BY date(date)";
+    var sql = "SELECT team, date(date) as date, AVG(score) as sentiment, COUNT(score) as count FROM tweets WHERE team = ? GROUP BY date(date)";
     var params = [req.params.team.toUpperCase()];
     tweets_query(sql, params, res);
 });
 router.get('/:team/tweetscore', function(req, res) {
-    let sql = "SELECT team, AVG(score) as sentiment, COUNT(score) as count FROM tweets WHERE team = ?";
+    var sql = "SELECT team, AVG(score) as sentiment, COUNT(score) as count FROM tweets WHERE team = ?";
     var params = [req.params.team.toUpperCase()];
     tweets_query(sql, params, res);
 });
 
 
 function tweets_query(sql, params, res) {
-    let file = '../data/data.db';
+    var file = '../data/data.db';
     query(file, sql, params, res);
 }
 function stats_query(sql, params, res) {
-    let file = '../data/data_team.db';
+    var file = '../data/data_team.db';
     query(file, sql, params, res);
 }
 
 function query(file, sql, params, response) {
-    let db = new sqlite3.Database(file, sqlite3.OPEN_READONLY,
+    var db = new sqlite3.Database(file, sqlite3.OPEN_READONLY,
     (err) => {
         if (err) {
             return console.error(err.message);
