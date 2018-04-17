25c4cf2eba25680c0e18ebe53bf04464224d7c40
unknown <bhavika@csu.fullerton.edu>
2018-04-16 21:44:06 -0700

    changes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,5 @@
 	
-	   // When the user clicks on <div>, open the popup
+		// When the user clicks on <div>, open the popup
             function popupFunction(url, bodyName){
             				var xmlHttp = new XMLHttpRequest();
             				xmlHttp.open( "GET", url , false ); // false for synchronous request
@@ -10,20 +10,20 @@
             function setResponse(responseText, bodyName){
             	var responseJSON = $.parseJSON(responseText);
             	var tr;
-		var tbody = document.getElementById(bodyName);
-		$(tbody).empty();
+				var tbody = document.getElementById(bodyName);
+				$(tbody).empty();
                 for (var i = 0; i < responseJSON.length; i++) {
                     tr = $('<tr/>');
-            	    tr.append("<td>" + responseJSON[i].num + "</td>");
-            	    tr.append("<td>" + responseJSON[i].name + "</td>");
-            	    tr.append("<td>" + responseJSON[i].pos + "</td>");
+            		tr.append("<td>" + responseJSON[i].num + "</td>");
+            		tr.append("<td>" + responseJSON[i].name + "</td>");
+            		tr.append("<td>" + responseJSON[i].pos + "</td>");
                     tr.append("<td>" + responseJSON[i].height + "</td>");
                     tr.append("<td>" + responseJSON[i].weight + "</td>");
-            	    var x = document.getElementById(bodyName);
-            	    $(x).append(tr);
+            		var x = document.getElementById(bodyName);
+            		$(x).append(tr);
                 }
             }
-	    // When the user clicks on tab show details
+			// When the user clicks on tab show details
 				function tabFunction(url, tabName){
                       		var xmlHttp = new XMLHttpRequest();
                       		xmlHttp.open( "GET", url , false ); // false for synchronous request
@@ -34,8 +34,8 @@
                 function settabResponse(response, tabName){
                       var responseJSON = $.parseJSON(response);
                       var tr;
-		      var tbody = document.getElementById(tabName);
-		      $(tbody).empty();
+					  var tbody = document.getElementById(tabName);
+					  $(tbody).empty();
                       for (var i = 0; i < responseJSON.length; i++) {
                       tr = $('<tr/>');
                       tr.append("<td>" + responseJSON[i].team + "</td>");
